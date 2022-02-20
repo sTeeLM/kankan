f9615b1f7db8116b79b5841107a885d9c2fc7067
sTeeLM <steel.mental@gmail.com>
2022-02-20 19:04:37 +0800

    update 3

--- kankan.py
+++ kankan.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import zipfile
 import os,sys,getopt,re
 import xml.dom.minidom
 import array
-import urllib
+import urllib.request, urllib.parse, urllib.error
 import json
 
 def usage() :
-    print"""Usage:
+    print("""Usage:
     kankan download <-v> <book id>
         donwload: download epub file which book is <xxx>, -v print progress
     kankan <command> [<options>] <epub file>
@@ -29,12 +29,12 @@ def usage() :
             -k | --key-pairs : an encrypted file path in epub, and path of plain context of the encrypted file,
                               for example: "OEBPS/Images/coverpage.jpeg:coverpage.jpeg,OEBPS/Images/image1.jpeg:image2.jpeg"               
             -v | --verbose : show target text content
-        decrypt: decrypt epub file
+        dedrm: dedrm epub file
             Options:
             -k | --key-pairs : an encrypted file path in epub, and path of plain context of the encrypted file,
                               for example: "OEBPS/Images/coverpage.jpeg:coverpage.jpeg,OEBPS/Images/image1.jpeg:image2.jpeg"
             -v | --verbose : print progress
-    """
+    """)
 
 def parse_cmd(argv) :
     cmd_options = {
@@ -43,7 +43,7 @@ def parse_cmd(argv) :
         'info' : {'opts' : 'v', 'lopts' : ['verbose']},
         'rename' : {'opts' : 'f:', 'lopts' : ['format=']},
         'analyse' : {'opts' : 'k:v', 'lopts' : ['key-pairs=', 'verbose']},
-        'decrypt' : {'opts' : 'k:vt', 'lopts' : ['key-pairs=', 'verbose', 'trunk']},
+        'dedrm' : {'opts' : 'k:vt', 'lopts' : ['key-pairs=', 'verbose', 'trunk']},
         }
     parsed_options = {
         'verbose' : 0,
@@ -58,7 +58,7 @@ def parse_cmd(argv) :
     if len(argv) <= 2:
         usage()
         sys.exit(1)
-    if cmd_options.has_key(argv[1]) :
+    if argv[1] in cmd_options :
         try:
             opts, args = getopt.getopt(argv[2:], cmd_options[argv[1]]['opts'], cmd_options[argv[1]]['lopts'])
             for opt,arg in opts :
@@ -105,7 +105,7 @@ def parse_keypairs(opts):
             opts['key-pairs'] = None
             return
         ret[iv.tostring()] = key
-        print "add key pair %s:%d from %s:%s" %(dump_array(iv), len(key), enc_file, plain_file)
+        print("add key pair %s:%d from %s:%s" %(dump_array(iv), len(key), enc_file, plain_file))
     opts['key-pairs'] = ret
 
 def read_epub_file(opts, filename) :
@@ -114,13 +114,13 @@ def read_epub_file(opts, filename) :
         with zipfile.ZipFile(opts['epub-file'], 'r') as zip_file :  
             ret = zip_file.read(filename)
     except zipfile.BadZipfile:
-        print '%s is not a zip file' % (opts['epub-file'])
+        print('%s is not a zip file' % (opts['epub-file']))
         ret = None
     except KeyError:
-        print 'no %s in %s' % (filename, opts['epub-file'])
+        print('no %s in %s' % (filename, opts['epub-file']))
         ret = None
     except IOError:
-        print 'error when read %s' % (opts['epub-file'])
+        print('error when read %s' % (opts['epub-file']))
         ret = None
     return ret
 
@@ -136,10 +136,10 @@ def list_epub_entries(opts):
                                    key=lambda x : x.file_size if opts['size'] else x.filename,
                                    reverse=opts['reverse'])
     except zipfile.BadZipfile:
-        print '%s is not a zip file' % (opts['epub-file'])
+        print('%s is not a zip file' % (opts['epub-file']))
         epub_entries = None
     except IOError:
-        print 'error when read %s' % (opts['epub-file'])
+        print('error when read %s' % (opts['epub-file']))
         epub_entries = None
     return epub_entries
 
@@ -148,14 +148,14 @@ def list_epub(opts) :
     if epub_entries is None:
         sys.exit(1)
     if opts['verbose'] == 0 :
-        print 'size\tname'
+        print('size\tname')
         for info in epub_entries :
-            print "%d\t%s" % (info.file_size, info.filename)
+            print("%d\t%s" % (info.file_size, info.filename))
     else :
-        print 'org_size\tcompress_size\ttype\tdate_time\tname'
+        print('org_size\tcompress_size\ttype\tdate_time\tname')
         for info in epub_entries :
             date_time = '%04d-%02d-%02d %02d:%02d:%02d' % info.date_time
-            print "%d\t%d\t%s\t%s\t%s" % (info.file_size, info.compress_size, info.compress_type, date_time, info.filename)
+            print("%d\t%d\t%s\t%s\t%s" % (info.file_size, info.compress_size, info.compress_type, date_time, info.filename))
     sys.exit(0)
         
         
@@ -185,7 +185,7 @@ def read_epub_meta(opts):
     if opf == None :
         opf = read_epub_file(opts, 'content.opf')       
     if opf == None :
-        print "no content.opf found!"
+        print("no content.opf found!")
         return None
     dom = xml.dom.minidom.parseString(opf)
     for node in dom.documentElement.getElementsByTagName('metadata')[0].childNodes:
@@ -207,22 +207,22 @@ def info_epub(opts):
     if epub_meta == None :
         sys.exit(1)
     if opts['verbose'] :
-        for key in epub_meta.keys() :
+        for key in list(epub_meta.keys()) :
             if key != 'meta' and key != 'identifier' :
-                print '%s: %s' % (key, epub_meta[key])
+                print('%s: %s' % (key, epub_meta[key]))
             elif  key == 'identifier' :
-                print 'identifier:'
-                for i in epub_meta['identifier'].keys() :
-                    print '\t%s:\t%s' % (i, epub_meta['identifier'][i])
+                print('identifier:')
+                for i in list(epub_meta['identifier'].keys()) :
+                    print('\t%s:\t%s' % (i, epub_meta['identifier'][i]))
             elif  key == 'meta' :
-                print 'meta:'
-                for i in epub_meta['meta'].keys() :
-                    print '\t%s:\t%s' % (i, epub_meta['meta'][i])
+                print('meta:')
+                for i in list(epub_meta['meta'].keys()) :
+                    print('\t%s:\t%s' % (i, epub_meta['meta'][i]))
                 
     else :
-        print 'title: %s' % (epub_meta['title'])
-        print 'publisher: %s' % (epub_meta['publisher'])
-        print 'creator: %s' % (epub_meta['creator'])
+        print('title: %s' % (epub_meta['title']))
+        print('publisher: %s' % (epub_meta['publisher']))
+        print('creator: %s' % (epub_meta['creator']))
     sys.exit(0)
 
 def rename_epub(opts) :
@@ -253,7 +253,7 @@ def rename_epub(opts) :
     old_path = os.path.abspath(opts['epub-file'])
     dir_path, old_name = os.path.split(old_path)
     new_path = os.path.join(dir_path ,new_name)
-    print "%s => %s" % (old_path, new_path)
+    print("%s => %s" % (old_path, new_path))
     os.rename(old_path, new_path)
     sys.exit(0) 
 
@@ -268,13 +268,13 @@ def gen_key(opts, enc_file, plain_file) :
             plain = None
     if plain == None or enc == None :
         return None
-    print "plain size %d" % len(plain)
-    print "encrypt size %d" % len(enc)
+    print("plain size %d" % len(plain))
+    print("encrypt size %d" % len(enc))
     # check plain file size
     diff_len = len(enc) - 16 - len(plain)
     if diff_len < 0 or diff_len >= 16 :
-        print "plain file size %d mismatch with encrypted one %d , should be [%d %d]" % \
-            (len(plain), len(enc), len(enc) - 16 - 15, len(enc) - 16)
+        print("plain file size %d mismatch with encrypted one %d , should be [%d %d]" % \
+            (len(plain), len(enc), len(enc) - 16 - 15, len(enc) - 16))
         return None
     # make key
     enc = array.array('B', enc)
@@ -286,8 +286,8 @@ def gen_key(opts, enc_file, plain_file) :
     for i in range(len(plain)):
         key.append(plain[i] ^ enc[i+16])
     iv = enc[0:16]
-    print 'key size %d' % (len(key))
-    print 'iv size %d' % (len(iv))
+    print('key size %d' % (len(key)))
+    print('iv size %d' % (len(iv)))
     return (key, iv)
 
 def read_encryption_xml(opts) :
@@ -307,25 +307,25 @@ def dump_array(av):
         ret_str += format(av[i], 'x')
     return ret_str
 
-def decrypt_content(enc, opts):
+def dedrm_content(enc, opts):
     enc = array.array('B', enc)
     plain = array.array('B')
     trunked = False
     if len(enc) < 16:
-        print "encrypted content too small %d < %d" %(len(enc), 16)
+        print("encrypted content too small %d < %d" %(len(enc), 16))
         return (None, None)
     s = len(enc) - 16
     iv = enc[0:16]
-    if opts['key-pairs'].has_key(iv.tostring()) :
+    if iv.tostring() in opts['key-pairs'] :
         key = opts['key-pairs'][iv.tostring()]
     else:
-        print "unknown iv %s!" %(dump_array(iv))
+        print("unknown iv %s!" %(dump_array(iv)))
         return (None, None)
     if len(enc) - 16 > len(key) and opts['trunk'] == 0:
-        print "encrypted content too long %d > %d" %(len(enc) - 16, len(key))
+        print("encrypted content too long %d > %d" %(len(enc) - 16, len(key)))
         return (None, None)
     elif len(enc) - 16 > len(key) and opts['trunk'] == 1:
-        print "encrypted content too long %d > %d, Trunked!" %(len(enc) - 16, len(key))
+        print("encrypted content too long %d > %d, Trunked!" %(len(enc) - 16, len(key)))
         s = len(key)
         trunked = True
     for i in range(s) :
@@ -356,44 +356,44 @@ def analyse_epub(opts):
     # search target file
     max_size = 0
     target_image = ''
-    for i in enc_list.keys() :
+    for i in list(enc_list.keys()) :
         if enc_list[i] != 'http://www.w3.org/2001/04/xmlenc#aes128-ctr' :
-            print "unknown algorithm %s of %s" % (nc_list[i], i)
+            print("unknown algorithm %s of %s" % (nc_list[i], i))
             sys.exit(1)
     for i in entries :
-        if i.file_size > max_size and enc_list.has_key(i.filename) :
+        if i.file_size > max_size and i.filename in enc_list :
             max_size = i.file_size
             target_image = i.filename
     target_image = target_image.encode('utf-8')
     # target_image must be an image file, and is big enough!
     if target_image != '' and max_size > 0 and is_image_file(target_image) :
-        print "found target image %s, size is %d" % (target_image, max_size)
+        print("found target image %s, size is %d" % (target_image, max_size))
     else :
-        print "target file not found!"
+        print("target file not found!")
     # find which encrypted text content has target file
     target_image = target_image.split('/')[-1:][0]
     target_text = ''
     trunked = False
-    for i in enc_list.keys() :
+    for i in list(enc_list.keys()) :
         if is_text_file(i.encode('utf-8')) :
             enc_txt = read_epub_file(opts, i.encode('utf-8'))
-            (plain_text, trunked) = decrypt_content(enc_txt, opts)
+            (plain_text, trunked) = dedrm_content(enc_txt, opts)
             if plain_text == None:
-                print "decrypt fail : %s" % (i)
+                print("dedrm fail : %s" % (i))
                 continue
             if plain_text.find(target_image) != -1:
                 target_text = i.encode('utf-8')
-                print "%s has %s" % (target_text, target_image)
+                print("%s has %s" % (target_text, target_image))
                 if(opts['verbose']) :
-                    print "content is \n%s" % (plain_text)
+                    print("content is \n%s" % (plain_text))
                 else :
-                    print "use -v to show content"
+                    print("use -v to show content")
                 break
     sys.exit(0)
 
-def decrypt_epub(opts) :
+def dedrm_epub(opts) :
     """
-    decrypt epub file
+    dedrm epub file
     """
     enc_list = read_encryption_xml(opts)
     entries = list_epub_entries(opts)
@@ -407,23 +407,23 @@ def decrypt_epub(opts) :
                         continue
                     content = read_epub_file(opts, ent.filename)
                     trunked = False
-                    if enc_list.has_key(ent.filename) :
-                        (plain, trunked)  = decrypt_content(content, opts)
+                    if ent.filename in enc_list :
+                        (plain, trunked)  = dedrm_content(content, opts)
                         if plain == None :
-                            print "decrypt fail : %s" % (ent.filename)
+                            print("dedrm fail : %s" % (ent.filename))
                             z_bad.writestr(ent.filename, content)
                             continue
                         if trunked :
-                            print "decrypt trunked : %s" % (ent.filename)
+                            print("dedrm trunked : %s" % (ent.filename))
                         if is_text_file(ent.filename.encode('utf-8')) :
                             # strip end null
                             plain = plain.rstrip('\0')
                         if opts['verbose'] :
-                            print "add decrypt : %s" % (ent.filename)
+                            print("add dedrm : %s" % (ent.filename))
                         z.writestr(ent.filename, plain)
                     else:
                         if opts['verbose'] :
-                            print "add plain : %s" % (ent.filename)
+                            print("add plain : %s" % (ent.filename))
                         z.writestr(ent.filename, content)
             z_bad.close()
         z.close()
@@ -431,10 +431,10 @@ def decrypt_epub(opts) :
     
 def save_url_to_file(urlstr, outfile):
     ret = True
-    print "%s ==> %s" %(urlstr, outfile)
+    print("%s ==> %s" %(urlstr, outfile))
     try:
         save_size = 0
-        fp = urllib.urlopen(urlstr.encode('utf-8'));
+        fp = urllib.request.urlopen(urlstr.encode('utf-8'));
         with open( outfile, 'wb') as fo :
             while True:
                 output_data = fp.read(1024)
@@ -444,7 +444,7 @@ def save_url_to_file(urlstr, outfile):
                 save_size += len(output_data)
                 sys.stdout.write("Download bytes: %d   \r" % (save_size) )
     except IOError:
-        print "can not save %s" %(urlstr)
+        print("can not save %s" %(urlstr))
         ret = False
     else:
         fp.close()
@@ -453,13 +453,13 @@ def save_url_to_file(urlstr, outfile):
 # http://www.duokan.com/store/v0/web/book/xxx
 def download_epub(opts):
     info_url = 'http://www.duokan.com/store/v0/web/book/' + opts['epub-file'];
-    print 'url is %s' %(info_url)
+    print('url is %s' %(info_url))
 
     try:
-        fp = urllib.urlopen(info_url);
+        fp = urllib.request.urlopen(info_url);
         book_info = fp.read()
     except IOError:
-        print "can not load %s" %(info_url)
+        print("can not load %s" %(info_url))
         sys.exit(1)
     else:
         fp.close()
@@ -469,13 +469,13 @@ def download_epub(opts):
         book_url = book_hash['book']['epub']
         coverpage_url = book_hash['book']['cover']
     except ValueError:
-        print "can not download book info, return %s" %(book_info)
+        print("can not download book info, return %s" %(book_info))
         sys.exit(1)
     except KeyError:
         if opts['verbose'] == 1:
-            print "book info load error, data is %s" %(book_hash)
+            print("book info load error, data is %s" %(book_hash))
         else:
-            print "book info load error, use -v see detail"
+            print("book info load error, use -v see detail")
         sys.exit(1)
 
     if coverpage_url[-2] == '!' and coverpage_url[-1] == 'm':
@@ -484,8 +484,8 @@ def download_epub(opts):
     book_name = book_url.split()[-1].split('/')[-1]
     coverpage_name = coverpage_url.split()[-1].split('/')[-1]
 
-    print 'book_url is %s' %(book_url)
-    print 'coverpage_url is %s' %(coverpage_url)
+    print('book_url is %s' %(book_url))
+    print('coverpage_url is %s' %(coverpage_url))
 
     save_url_to_file(coverpage_url, coverpage_name)
     save_url_to_file(book_url, book_name)
@@ -497,7 +497,7 @@ def main(argv) :
     if opts['key-pairs'] != None:
         parse_keypairs(opts)
         if opts['key-pairs'] == None:
-            print "parse key pairs error!"
+            print("parse key pairs error!")
             sys.exit(1)
     if cmd == 'download' :
         download_epub(opts)
@@ -509,8 +509,8 @@ def main(argv) :
         rename_epub(opts)
     elif cmd == 'analyse' :
         analyse_epub(opts)
-    elif cmd == 'decrypt' :
-        decrypt_epub(opts)
+    elif cmd == 'dedrm' :
+        dedrm_epub(opts)
     else:
         sys.exit(1)
 
